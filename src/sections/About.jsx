51f5c83c64e4f0fadcf5d1d9c0cadea919ee1354
26041012 <s90387272@gmail.com>
2025-07-06 17:41:52 +0530

    Fix case sensitivity issue in Frameworks import

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -2,7 +2,7 @@ import { useRef, useState } from "react";
 import Card from "../components/Card";
 import { Globe } from "../components/globe";
 import CopyEmailButton from "../components/CopyEmailButton";
-import { Frameworks } from "../components/FrameWorks";
+import { Frameworks } from "../components/Frameworks";
 
 const TiltCard = ({ children, className }) => {
   const [tilt, setTilt] = useState({ x: 0, y: 0 });
